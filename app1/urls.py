c1e73e0b435269c6fd04e2f5ced9369a8773c41f
arvydasg <azegaspa@gmail..com>
2021-08-30 11:50:59 +0300

    made django print out something

--- app1/urls.py
+++ app1/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
+from django.urls import path
+from . import views             # from relative import views
+
+app_name = "app1"               # good to use, listen 14:07 https://www.youtube.com/watch?v=yD0_1DPmfKM&list=PLQVvvaa0QuDe9nqlirjacLkBYdgc2inh3
+
+# all paths lead to this urls.py now
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", views.homepage, name="homepage"),
 ]
