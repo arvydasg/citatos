cf33e5b1b1402f34945a51381d6721d3079b1252
arvydasg <azegaspa@gmail..com>
2021-08-30 13:22:25 +0300

    no need for separate name_last name

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import
 
 # Register your models here.
+
+admin.site.register(Quote)
+admin.site.register(Author)
+admin.site.register(Category)
--- app1/models.py
+++ app1/models.py
@@ -14,14 +14,13 @@ class Quote(models.Model):
         ordering = ['published']
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=30, null=True)
-    last_name = models.CharField(max_length=30, null=True)
+    name = models.CharField(max_length=50, null=True)
     bio = models.TextField()
     birth_date = models.CharField(max_length=30, null=True)
     death_date = models.CharField(max_length=30, null=True)
 
     def __str__(self):
-        return self.last_name
+        return self.name
 
     class Meta:
         ordering = ['last_name']
