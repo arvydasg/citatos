56ac90bf44d447feb510211642be58aa90216c12
arvydasg <azegaspa@gmail..com>
2021-08-30 12:15:08 +0300

    added model?

--- app1/models.py
+++ app1/models.py
@@ -2,11 +2,9 @@ from django.db import models
 
 class Citata(models.Model):
     citata_author = models.CharField(max_length=255)
-    citata_content = models.TextField()
+    citata_category = models.CharField(max_length=255)
+    citata_quote = models.TextField()
     citata_published = models.DateTimeField("date published")
 
     def __str__(self):
         return self.citata_title
-
-    class Meta:
-        ordering = ['-date_added']
