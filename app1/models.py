e3d9a3fb38b0fa4cd9ca2d004356b7f9bf084422
arvydasg <azegaspa@gmail..com>
2021-08-30 12:05:45 +0300

    ka manai?

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Citata(models.Model):
+    citata_author = models.CharField(max_length=255)
+    citata_content = models.TextField()
+    citata_published = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.citata_title
+
+    class Meta:
+        ordering = ['-date_added']
