027696e1dee33b20e4c34d68e5f496cad779dc5e
arvydasg <azegaspa@gmail..com>
2021-08-30 13:17:28 +0300

    model attempt

--- app1/models.py
+++ app1/models.py
@@ -1,10 +1,36 @@
 from django.db import models
+from datetime import datetime
 
 class Quote(models.Model):
-    quote_author = models.CharField(max_length=100)   # foreign key
-    quote_category = models.CharField(max_length=100) # foreign key
-    quote_text = models.TextField()
-    quote_published = models.DateTimeField("date published")
+    author = models.ForeignKey(Author, null=True, on_delete= models.SET_NULL)
+    category = models.ManyToManyField(Category)
+    text = models.TextField(null=True)
+    published = models.DateTimeField("date published", default=datetime.now())
 
     def __str__(self):
         return self.quote_text
+
+    class Meta:
+        ordering = ['published']
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30, null=True)
+    last_name = models.CharField(max_length=30, null=True)
+    bio = models.TextField()
+    birth_date = models.CharField(max_length=30, null=True)
+    death_date = models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return self.last_name
+
+    class Meta:
+        ordering = ['last_name']
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
