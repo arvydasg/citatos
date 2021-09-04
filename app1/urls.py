52295219db0b9e108f2639753c3a36187883152b
arvydasg <azegaspa@gmail.com>
2021-09-04 15:32:27 +0300

    some colors

--- app1/urls.py
+++ app1/urls.py
@@ -5,7 +5,9 @@ app_name = "app1"
 
 # all paths lead to this urls.py now
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("home/", views.index, name="index"),
     path("books/", views.books, name="books"),
-    # path("authors/", authors.books, name="authors"),
+    path("authors/", views.authors, name="authors"),
+    path("categories/", views.categories, name="categories"),
+    path("about/", views.about, name="about"),
 ]
--- app1/views.py
+++ app1/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Quote
 from .models import Book
 from .models import Author
+from .models import Category
 
 # Create your views here.
 def index(request):
@@ -15,7 +16,18 @@ def books(request):
                   template_name="app1/books.html",
                   context={"book": Book.objects.all})
 
-# def authors(request):
-#     return render(request=request,
-#                   template_name="app1/authors.html",
-#                   context={"author": Author.objects.all})
+def authors(request):
+    return render(request=request,
+                  template_name="app1/authors.html",
+                  context={"author": Author.objects.all})
+
+def categories(request):
+    return render(request=request,
+                  template_name="app1/categories.html",
+                  context={"category": Category.objects.all})
+
+def about(request):
+    return render(request=request,
+                  template_name="app1/about.html",)
+
+
