64947157254905993b9d8cad229d235381f2ba10
arvydasg <azegaspa@gmail.com>
2021-09-04 10:03:09 +0300

    added a few books, sucessful migration

--- app1/models.py
+++ app1/models.py
@@ -16,6 +16,9 @@ class Author(models.Model):
 class Book(models.Model):
     name = models.CharField(max_length=100, null=True)
     author = models.ForeignKey(Author, null=True, on_delete= models.SET_NULL)
+    publihed = models.CharField(max_length=30, null=True)
+    pages = models.CharField(max_length=30, null=True)
+    read_in = models.CharField(max_length=100, null=True)
     summary = models.TextField()
 
     def __str__(self):
@@ -38,7 +41,9 @@ class Quote(models.Model):
     author = models.ForeignKey(Author, null=True, on_delete= models.SET_NULL)
     category = models.ManyToManyField(Category)
     text = models.TextField(null=True)
+    book = models.ForeignKey(Book, null=True, on_delete= models.SET_NULL)
     published = models.DateTimeField("date published", auto_now_add=True)
+
 
     def __str__(self):
         return self.text
