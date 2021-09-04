e2e05a4dbcf27fc0abd6602d148f8a6d98cf8f5d
arvydasg <azegaspa@gmail.com>
2021-09-04 14:42:46 +0300

    so cool to be able to add fields to admin panel in seconds!!

--- app1/models.py
+++ app1/models.py
@@ -5,7 +5,10 @@ class Author(models.Model):
     name = models.CharField(max_length=50, null=True)
     bio = models.TextField()
     birth_date = models.CharField(max_length=30, null=True)
-    death_date = models.CharField(max_length=30, null=True)
+    birth_place = models.CharField(max_length=100, null=True, blank=True)
+    death_date = models.CharField(max_length=30, null=True, blank=True)
+    death_place = models.CharField(max_length=100, null=True, blank=True)
+    cause_of_death = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.name
